8e1a3a91f545e9d8b86f6c19cf5bea15aee561e5
Matteo Giordano <matteo.giordano@cloudesire.com>
2018-04-11 13:10:15 +0200

    Create service route

--- src/main/java/com/github/vaibhavsinha/kong/api/admin/ServiceService.java
+++ src/main/java/com/github/vaibhavsinha/kong/api/admin/ServiceService.java
@@ -1,8 +1,11 @@
 package com.github.vaibhavsinha.kong.api.admin;
 
+import com.github.vaibhavsinha.kong.model.admin.service.Route;
 import com.github.vaibhavsinha.kong.model.admin.service.Service;
 
 public interface ServiceService
 {
     Service createService(Service request);
+
+    Route createServiceRoute(String service, Route request);
 }
