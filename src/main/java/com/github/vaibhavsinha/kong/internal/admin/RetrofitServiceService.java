81c4635ec12b892780cfb408f7cdcac1771c1b32
Matteo Giordano <matteo.giordano@cloudesire.com>
2018-04-11 13:10:15 +0200

    Create service route

--- src/main/java/com/github/vaibhavsinha/kong/internal/admin/RetrofitServiceService.java
+++ src/main/java/com/github/vaibhavsinha/kong/internal/admin/RetrofitServiceService.java
@@ -1,12 +1,17 @@
 package com.github.vaibhavsinha.kong.internal.admin;
 
+import com.github.vaibhavsinha.kong.model.admin.service.Route;
 import com.github.vaibhavsinha.kong.model.admin.service.Service;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface RetrofitServiceService
 {
     @POST("services")
     Call<Service> createService(@Body Service request);
+
+    @POST("services/{service}/routes")
+    Call<Route> createServiceRoute(@Path("service") String service, @Body Route request);
 }
---
+++ src/main/java/com/github/vaibhavsinha/kong/model/admin/service/Route.java
@@ -0,0 +1,17 @@
+package com.github.vaibhavsinha.kong.model.admin.service;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Route
+{
+    private String id;
+
+    private List<String> paths;
+
+    @SerializedName("strip_path")
+    private boolean stripPath;
+}
