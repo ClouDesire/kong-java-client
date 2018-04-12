476199b8d40f629d56f9bbbd298d3fe7c5160929
Matteo Giordano <matteo.giordano@cloudesire.com>
2018-04-12 17:49:17 +0200

    Create service plugin

--- src/main/java/com/github/vaibhavsinha/kong/api/admin/ServiceService.java
+++ src/main/java/com/github/vaibhavsinha/kong/api/admin/ServiceService.java
@@ -1,11 +1,14 @@
 package com.github.vaibhavsinha.kong.api.admin;
 
+import com.github.vaibhavsinha.kong.model.admin.plugin.Plugin;
 import com.github.vaibhavsinha.kong.model.admin.service.Route;
 import com.github.vaibhavsinha.kong.model.admin.service.Service;
 
 public interface ServiceService
 {
     Service createService(Service request);
+
+    Plugin createServicePlugin(String service, Plugin request);
 
     Route createServiceRoute(String service, Route request);
 }
--- src/main/java/com/github/vaibhavsinha/kong/internal/admin/RetrofitServiceService.java
+++ src/main/java/com/github/vaibhavsinha/kong/internal/admin/RetrofitServiceService.java
@@ -1,5 +1,6 @@
 package com.github.vaibhavsinha.kong.internal.admin;
 
+import com.github.vaibhavsinha.kong.model.admin.plugin.Plugin;
 import com.github.vaibhavsinha.kong.model.admin.service.Route;
 import com.github.vaibhavsinha.kong.model.admin.service.Service;
 import retrofit2.Call;
@@ -11,6 +12,9 @@ public interface RetrofitServiceService
 {
     @POST("services")
     Call<Service> createService(@Body Service request);
+
+    @POST("services/{service}/plugins")
+    Call<Plugin> createServicePlugin(@Path("service") String service, @Body Plugin request);
 
     @POST("services/{service}/routes")
     Call<Route> createServiceRoute(@Path("service") String service, @Body Route request);
